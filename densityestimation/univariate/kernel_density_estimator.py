e50fa793cbf1ee8f9050429bc25bf69c500f3a32
Grzegorz <g.w.mika@gmail.com>
2020-12-09 17:03:21 +0100

    custom exceptions, base class

--- densityestimation/univariate/kernel_density_estimator.py
+++ densityestimation/univariate/kernel_density_estimator.py
@@ -1,16 +1,16 @@
 from densityestimation import kernels
-from densityestimation.utils.gpuoptional import array_module
+from densityestimation.base_estimator import BaseEstimator
 from densityestimation.utils.validators import validate_kernel, validate_bandwidth
 
 
-class KernelDensityEstimator:
+class KernelDensityEstimator(BaseEstimator):
     def __init__(self, kernel, bandwidth, module=None):
+        super().__init__(module)
         self.validate_parameters(kernel, bandwidth)
 
         self.kernel = kernels.KERNELS[kernel]
         self.X = None
         self.bandwidth = bandwidth
-        self.xp = array_module(module)
 
     @staticmethod
     def validate_parameters(kernel, bandwidth):
@@ -18,7 +18,12 @@ class KernelDensityEstimator:
         validate_bandwidth(bandwidth)
 
     def fit(self, X, y=None):
+        super().fit(X, y)
         self.X = self.xp.asarray(X).reshape(-1, 1)
 
-    def predict(self, X):
+    def score_sample(self, X):
+        super().score_sample(X)
         return self.xp.mean(self.kernel((X - self.X) / self.bandwidth, module=self.xp), axis=0) / self.bandwidth
+
+    def sample(self, n_samples, random_state=None):
+        super().sample(n_samples, random_state)
